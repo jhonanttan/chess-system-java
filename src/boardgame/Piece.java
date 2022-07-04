e4621fb58242d2e8838d4e35eac72889efc77b1c
Jhon Willyan <jhonanttan1@gmail.com>
2022-07-03 21:38:17 -0300

    Camada de xadrez e print do tabuleiro

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,5 +1,25 @@
 package boardgame;
 
-public class Piece {
+public class Piece { //peça
+	
+	protected Position position;
+	private Board board;
+	
+	
+	public Piece(Board board) {
+		this.board = board;
+		position = null;
+	}
 
+
+	protected Board getBoard() {
+		return board;
+	}
+
+	
+	
+	
+	
+	
+	
 }
